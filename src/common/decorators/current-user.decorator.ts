ed5ce166887f164f67012edfae51a5ccd8f8b96c
Sahadat Hossen <sahadatjh@gmail.com>
2026-09-15 12:28:05 +0600

    Feat: enhance CurrentUser decorator to support optional data extraction from user object

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -2,8 +2,9 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '../../modules/users/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext): User => {
+  (data: keyof User | undefined, ctx: ExecutionContext): User | User[keyof User] | undefined => {
     const request = ctx.switchToHttp().getRequest();
-    return request.user;
+    const user = request.user as User | undefined;
+    return data ? user?.[data] : user;
   },
 );
