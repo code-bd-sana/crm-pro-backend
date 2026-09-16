90971878ea62bda878c800a34aad4b8a00b1381a
Sahadat Hossen <sahadatjh@gmail.com>
2026-09-16 12:23:19 +0600

    fix: add logging for unhandled exceptions in HttpExceptionFilter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -14,6 +14,10 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
+    if (!(exception instanceof HttpException)) {
+      console.error(`Unhandled ${request.method} ${request.url}:`, exception);
+    }
+
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
